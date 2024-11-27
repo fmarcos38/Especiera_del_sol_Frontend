d1baa6f5bcc78a65eae36986ee055e556aad2de5
fmarcos38 <fmarcos_23@hotmail.com>
2024-11-27 16:00:44 -0300

    remito pdf

--- src/Componentes/Remito/index.jsx
+++ src/Componentes/Remito/index.jsx
@@ -175,11 +175,11 @@ function Remito({
                                 {/* <img src={textoLogo} alt='' className='texto-logo' /> */}
                                 <img src={logoRemito} alt='' className='logo-remito' />
                                 <p>De Gustavo Matusovsky</p>
-                                <p>11 4199 7200</p>
-                                <p>11 5951 0493</p>
+                                <p>Cel. +54. 11 4199 7200</p>
+                                <p>Cel. +54. 11 5951 0493</p>
                                 <p>info@especieradelsol.com</p>
                                 <p>www.especieradelsol.com</p>
-                                <p style={{ fontSize: '10px' }}>IVA RESPONSABLE INSCRIPTO</p>
+                                <p style={{ fontSize: '13px' }}>IVA RESPONSABLE INSCRIPTO</p>
                             </div>
                             {/* cont X */}
                             <div className='cont-remito-sup-info-X'>
@@ -197,14 +197,9 @@ function Remito({
                         {/* cont sup Derecho */}
                         <div className='cont-remito-sup-derecho'>
                             <div className='cont-remito-derecho-SUP'>
-                                <div style={{ width: '90%',display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <p className='derecho-SUP-titulo'>REMITO</p>
-                                    <p className='num-remito'>N° {nuevoNumeroRemito}</p>
-                                </div>
-                                <div style={{ width: '90%',display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                                    <p className='fecha-remito'>Fecha: </p>
-                                    <p>{fechaArg(fechaAct)}</p>
-                                </div>
+                                <p className='derecho-SUP-titulo'>REMITO</p>
+                                <p className='num-remito'>N° {nuevoNumeroRemito}</p>
+                                <p className='fecha-remito'>Fecha: {fechaArg(fechaAct)}</p>
                             </div>
                             <div className='cont-remito-derecho-INF'>
                                 <div className='cont-remito-derecho-INF-izq'>
